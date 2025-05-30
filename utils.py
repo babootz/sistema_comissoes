1cd1b27937bef03c6cc5ca44c9b51bdc0b3b93ce
caio.cfcs <133669620+babootz@users.noreply.github.com>
2025-05-30 10:37:38 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -28,9 +28,3 @@ def baixar_arquivo(data, nome_arquivo):
     b64 = base64.b64encode(data).decode()
     href = f'<a href="data:application/octet-stream;base64,{b64}" download="{nome_arquivo}">Clique aqui para baixar</a>'
     return href
-
-import streamlit as st
-
-def load_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
